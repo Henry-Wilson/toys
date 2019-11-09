cef6d011764c5ca9ecbc060ec5ac9f02fd81d866
Henry Wilson <henry.wilson.17@cnu.edu>
2019-11-08 22:04:56 -0500

    added m_choose_n func. Equivalent double implementation seems bad.

--- c-math/mathtoys.c
+++ c-math/mathtoys.c
@@ -2,6 +2,7 @@
  * Some mathematical toys.
  */
 
+//Simple recursive implementation of a fibonacci sequence.
 void fibonacci(int* pA, int* pB, int term){
     //We ought to terminate on Zero.
     if(!term){
@@ -12,4 +13,68 @@ void fibonacci(int* pA, int* pB, int term){
     *pB = temp;
     fibonacci(pA, pB, term - 1);
     return;
+}
+
+//very efficient m_choose_n algorithm running in unsigned long long space.
+unsigned long long int m_choose_n(int m, int n){
+    //Start with a result equal to identity.
+    unsigned long long int result = 1;
+
+    /*
+     * range to multiply by: [m to (m-n))
+     * range to divide by: [n to 1)
+     * 
+     * We ought to check before division if
+     * we are properly divisible. Does modular
+     * arithmetic work in C natively? One must
+     * imagine so, given that it follows
+     * naturally from the addition functions of
+     * all common ALUs.
+     */
+
+    // M Iterator
+    int miter = m;
+    // N Iterator
+    int niter = n;
+
+    //Multiply by each miter. Divide when possible.
+    while ( miter > (m-n) ) {
+        //Do we want to start smallest first or largest first?
+        result = result * miter; //Largest first for now
+        //Divide by niter as many times as possible.
+        while ( niter > 1 ) {
+            //If there's NOT a remainder
+            if ( result % niter == 0) {
+                result = result / niter;
+            }
+            else{
+                break;
+            }
+            niter--;
+        }
+        miter--;
+    }
+
+    return result;
+}
+
+//Just for comparison. This is the bad function.
+//We'll try a speed comparison some time.
+double bad_m_choose_n( double m, double n ){
+    double fm = 1;
+    double fmn = 1;
+    double fn = 1;
+
+    for(int i = 2; i <= m; i++){
+        fm = fm * i;
+    }
+    for(int i = 2; i <= n; i++){
+        fn = fn * i;
+    }
+    for(int i = 2; i <= (m-n); i++){
+        fmn = fmn * i;
+    }
+
+    double result = fm / (fmn * fn);
+    return result; 
 }
--- c-math/mathtoys.h
+++ c-math/mathtoys.h
@@ -4,5 +4,11 @@
 /* Recursive pointer-based fibonacci implementation */
 void fibonacci(int* pA, int* pB, int term);
 
+/* iterative and efficient m_choose_n algorithm */
+unsigned long long int m_choose_n(int m, int n);
+
+/* 'bad' m_choose_n algorithm for comparison */
+double bad_m_choose_n(double m, double n);
+
 #endif // mathtoys_h_INCLUDED
 
