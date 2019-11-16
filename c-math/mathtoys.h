4d4110609687f00be90b3b8a83be01dad0479125
Henry Wilson <43455296+Henry-Wilson@users.noreply.github.com>
2019-11-16 13:28:33 -0500

    Removed outdated function declarations

--- c-math/mathtoys.h
+++ c-math/mathtoys.h
@@ -10,17 +10,11 @@ void fibonacci(int* pA, int* pB, int term);
 /* elegant fibonacci function */
 int fib(int n);
 
-/* iterative and efficient m_choose_n algorithm */
-MNTYPE m_choose_n(int m, int n);
-
 /* Better function. Does not rely on modulo */
 MNTYPE best_choose(int m, int n);
 
 /* 'bad' m_choose_n algorithm for comparison */
 double bad_m_choose_n(double m, double n);
-
-/* hybrid function. should be very strong. */
-double hyb_m_choose_n(double m, double n);
 
 #endif // mathtoys_h_INCLUDED
 
